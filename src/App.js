7b98352076f29c71122957d78119118cc4f8c03a
NesrineSaidi1 <saidinesrine888@gmail.com  git config --global user.name NesrineSaidi1git commit -m first>
2023-01-31 20:08:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
 function App() {
+  const [users, setusers] = useState([]);
+  useEffect(() => {
+    try {
+      axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+        setusers(res.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="body">
+        {users.map((el) => (
+          <Card className="box" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>{el.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                {el.username}
+              </Card.Subtitle>
+              <Card.Text>{el.company.name}</Card.Text>
+              <Card.Link
+                style={{ textDecoration: "none", color: "white" }}
+                href="#"
+              >
+                {el.website}
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
